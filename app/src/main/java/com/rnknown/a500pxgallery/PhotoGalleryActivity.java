2d95b95bdcdfa176e0d6446911a132c89fb18c3b
Ruslan Gilemzianov <Ruslan Gilemzianov>
2016-07-15 01:31:26 +0300

    Initial commit

---
+++ app/src/main/java/com/rnknown/a500pxgallery/PhotoGalleryActivity.java
@@ -0,0 +1,13 @@
+package com.rnknown.a500pxgallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoGalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_gallery);
+    }
+}
