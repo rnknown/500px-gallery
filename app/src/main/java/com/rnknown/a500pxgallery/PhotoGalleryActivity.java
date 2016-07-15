64ebaa50e953d1dc57c975df3e82d11ac85989bc
rnknown <ruslan.gmz@gmail.com>
2016-07-15 13:01:43 +0300

    changed logic of PhotoGalleryActivity class realization

--- app/src/main/java/com/rnknown/a500pxgallery/PhotoGalleryActivity.java
+++ app/src/main/java/com/rnknown/a500pxgallery/PhotoGalleryActivity.java
@@ -1,13 +1,14 @@
 package com.rnknown.a500pxgallery;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PhotoGalleryActivity extends AppCompatActivity {
+public class PhotoGalleryActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_photo_gallery);
+    protected Fragment createFragment () {
+
+        return PhotoGalleryFragment.newInstance();
     }
 }
