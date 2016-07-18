9125fef4462103f1871c467723143aa558ed9bae
rnknown <ruslan.gmz@gmail.com>
2016-07-18 19:45:12 +0300

    added unnecessary packages

--- app/src/main/java/com/rnknown/a500pxgallery/PhotoGalleryActivity.java
+++ app/src/main/java/com/rnknown/a500pxgallery/PhotoGalleryActivity.java
@@ -1,8 +1,6 @@
 package com.rnknown.a500pxgallery;
 
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
 
